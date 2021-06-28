69845cee0c1598e0eb16aa80b561572de617a20f
relam10 <alejandro2210lp@gmail.com>
2021-06-28 18:53:05 +0200

    improvements

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const healthbar = document.querySelectorAll(".vidas");
+const darkButton = document.querySelector(".dark-mode");
+const body = document.querySelector("body");
 
 healthbar.forEach((vida) => {
   vida.textContent = "❤".repeat(vida.textContent);
+});
+
+darkButton.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  darkButton.classList.toggle("dark-button");
 });
