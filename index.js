a3a9afe2f24dc4417757be0400b5de55da046bc7
relam10 <alejandro2210lp@gmail.com>
2021-06-28 19:05:44 +0200

    improvements

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const healthbar = document.querySelectorAll(".vidas");
-const darkButton = document.querySelector(".dark-mode");
-const body = document.querySelector("body");
 
 healthbar.forEach((vida) => {
   vida.textContent = "❤".repeat(vida.textContent);
 });
 
-darkButton.addEventListener("click", () => {
-  body.classList.toggle("dark");
-  darkButton.classList.toggle("dark-button");
-});
+const daysDiv = document.querySelector("#days");
+const hoursDiv = document.querySelector("#hours");
+const minutesDiv = document.querySelector("#min");
+const secondsDiv = document.querySelector("#seconds");
+
+const countDown = () => {
+  // Set Desired Date
+  const goalDate = "15 Jul 2021";
+
+  const desiredGoalDate = new Date(goalDate);
+  const currentDate = new Date();
+
+  const totalSeconds = (desiredGoalDate - currentDate) / 1000;
+
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 24;
+  const seconds = Math.floor(totalSeconds % 24);
+
+  daysDiv.textContent = days;
+  hoursDiv.textContent = hours;
+  secondsDiv.textContent = seconds;
+  minutesDiv.textContent = minutes;
+};
+
+// Initial Call
+countDown();
+
+setInterval(countDown, 1000);
